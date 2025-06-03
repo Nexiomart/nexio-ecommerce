b22fe191762f044cd96d711a54ddfa08347d7142
Riyansh Prakash <riyanshprakash007@gmail.com>
2025-06-03 18:24:23 +0530

    remove header part

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,38 +1,83 @@
 const AWS = require('aws-sdk');
-
+const { v4: uuidv4 } = require('uuid');
 const keys = require('../config/keys');
 
-exports.s3Upload = async image => {
+exports.s3Upload = async (image) => {
   try {
     let imageUrl = '';
     let imageKey = '';
 
-    if (!keys.aws.accessKeyId) {
-      console.warn('Missing aws keys');
+    if (!keys.aws.accessKeyId || !keys.aws.secretAccessKey) {
+      console.warn('Missing AWS access keys');
+      return { imageUrl, imageKey };
     }
 
     if (image) {
-      const s3bucket = new AWS.S3({
+      const s3 = new AWS.S3({
         accessKeyId: keys.aws.accessKeyId,
         secretAccessKey: keys.aws.secretAccessKey,
-        region: keys.aws.region
+        region: keys.aws.region,
       });
+
+      const uniqueKey = `${uuidv4()}-${image.originalname}`;
 
       const params = {
         Bucket: keys.aws.bucketName,
-        Key: image.originalname,
+        Key: uniqueKey,
         Body: image.buffer,
-        ContentType: image.mimetype
+        ContentType: image.mimetype,
+        // ACL: 'public-read', // Optional: remove if you want private files
       };
 
-      const s3Upload = await s3bucket.upload(params).promise();
+      const s3Upload = await s3.upload(params).promise();
 
       imageUrl = s3Upload.Location;
-      imageKey = s3Upload.key;
+      imageKey = s3Upload.Key;
     }
 
     return { imageUrl, imageKey };
   } catch (error) {
+    console.error('S3 Upload Error:', error);
     return { imageUrl: '', imageKey: '' };
   }
 };
+
+
+// const AWS = require('aws-sdk');
+
+// const keys = require('../config/keys');
+
+// exports.s3Upload = async image => {
+//   try {
+//     let imageUrl = '';
+//     let imageKey = '';
+
+//     if (!keys.aws.accessKeyId) {
+//       console.warn('Missing aws keys');
+//     }
+
+//     if (image) {
+//       const s3bucket = new AWS.S3({
+//         accessKeyId: keys.aws.accessKeyId,
+//         secretAccessKey: keys.aws.secretAccessKey,
+//         region: keys.aws.region
+//       });
+
+//       const params = {
+//         Bucket: keys.aws.bucketName,
+//         Key: image.originalname,
+//         Body: image.buffer,
+//         ContentType: image.mimetype
+//       };
+
+//       const s3Upload = await s3bucket.upload(params).promise();
+
+//       imageUrl = s3Upload.Location;
+//       imageKey = s3Upload.key;
+//     }
+
+//     return { imageUrl, imageKey };
+//   } catch (error) {
+//     return { imageUrl: '', imageKey: '' };
+//   }
+// };
